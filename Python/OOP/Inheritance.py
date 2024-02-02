bdda52260edbd9e0756e923b786b574c4c6298fc
Stanislaus <flysirin@gmail.com>
2024-02-02 11:54:06 +0400

    add Inheritance

--- Python/OOP/Inheritance.py
+++ Python/OOP/Inheritance.py
@@ -132,7 +132,6 @@
 # (3)
 
 
-
 # Создайте класс PermissionMixin, который будет иметь следующие методы:
 # __init__(self): метод инициализации, который создает множество permissions для хранения разрешений.
 # В него мы будем сохранять действия, которые будут доступны пользователям, например Чтение, Запись, Выполнение и т.д.
@@ -204,17 +203,198 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 # (4)
+# Класс DictMixin представляет собой миксин, который добавляет в класс, наследующий его, метод to_dict().
+# Этот метод позволяет преобразовать объект в словарь.
+# Внутри класса DictMixin вы можете создавать сколько угодно служебных методов и атрибутов, которые помогут
+# вам справиться с задачей. Главное, это реализовать метод to_dict(), он являться точкой входа для взаимодействия с
+# вашим миксином и он должен вернуть представление вашего объекта в виде словаря. Обратите внимание на вложенность атрибутов.
 
 
+# class DictMixin:
+#     excludes_types = (int, float, str)
+#
+#     def to_dict(self):
+#         return eval(self.__repr__())
+#
+#     def __repr__(self):
+#         return str(self.__dict__)
+#
+
+####
+
+# import json
+#
+# class DictMixin:
+#
+#     def to_dict(self):
+#         a = json.dumps(self.__dict__, indent=2, default=lambda x: x.__dict__)
+#         s = json.loads(a)
+#         return s
+
+####
+
+#
+# class Phone(DictMixin):
+#     def __init__(self, number):
+#         self.number = number
+#
+#
+# class Person(DictMixin):
+#     def __init__(self, name, age, address):
+#         self.name = name
+#         self.age = age
+#         self.address = address
+#
+#
+# class Address(DictMixin):
+#     def __init__(self, street, city, state, zip_code):
+#         self.street = street
+#         self.city = city
+#         self.state = state
+#         self.zip_code = zip_code
+#
+#
+# class Company(DictMixin):
+#     def __init__(self, name, address):
+#         self.name = name
+#         self.address = address
+#
+#
+# address = Address("123 Main St", "Anytown", "CA", "12345")
+# john_doe = Person("John Doe", 30, address)
+#
+# john_doe_dict = john_doe.to_dict()
+# print(john_doe_dict)
+# print(type(john_doe_dict))
+#
+#
+# assert john_doe_dict == {'name': 'John Doe', 'age': 30, 'address': {'street': '123 Main St', 'city': 'Anytown', 'state': 'CA', 'zip_code': '12345'}}
+#
+# address = Address("123 Main St", "Albuquerque", "NM", "987654")
+# assert address.to_dict() == {
+#     'street': '123 Main St',
+#     'city': 'Albuquerque',
+#     'state': 'NM',
+#     'zip_code': '987654'
+# }
+# walter = Person("Walter White", 30, address)
+# assert walter.to_dict() == {'address': {'city': 'Albuquerque',
+#                                         'state': 'NM',
+#                                         'street': '123 Main St',
+#                                         'zip_code': '987654'},
+#                             'age': 30,
+#                             'name': 'Walter White'}
+#
+# walter_phone = Phone("555-1234")
+# walter.phone = walter_phone
+# assert walter.to_dict() == {'address': {'city': 'Albuquerque',
+#                                         'state': 'NM',
+#                                         'street': '123 Main St',
+#                                         'zip_code': '987654'},
+#                             'age': 30,
+#                             'name': 'Walter White',
+#                             'phone': {'number': '555-1234'}}
+#
+# company_address = Address("3828 Piermont Dr", "Albuquerque", "NM", "12345")
+# company = Company("SCHOOL", company_address)
+#
+# assert company.to_dict() == {'address': {'city': 'Albuquerque',
+#                                          'state': 'NM',
+#                                          'street': '3828 Piermont Dr',
+#                                          'zip_code': '12345'},
+#                              'name': 'SCHOOL'}
+#
+# jesse_address = Address("456 Oak St", "Albuquerque", "NM", "12345")
+# jesse = Person("Jesse Bruce Pinkman", 27, jesse_address)
+# jesse.phone = Phone("555-5678")
+#
+# fring = Person("Gustavo Fring", 55, Address("Los Pollos Hermanos", "Albuquerque", "NM", "12345"))
+# fring.friends = [walter, jesse]
+# print(fring.to_dict())
+# assert fring.to_dict() == {'address': {'city': 'Albuquerque',
+#                                        'state': 'NM',
+#                                        'street': 'Los Pollos Hermanos',
+#                                        'zip_code': '12345'},
+#                            'age': 55,
+#                            'friends': [{'address': {'city': 'Albuquerque',
+#                                                     'state': 'NM',
+#                                                     'street': '123 Main St',
+#                                                     'zip_code': '987654'},
+#                                         'age': 30,
+#                                         'name': 'Walter White',
+#                                         'phone': {'number': '555-1234'}},
+#                                        {'address': {'city': 'Albuquerque',
+#                                                     'state': 'NM',
+#                                                     'street': '456 Oak St',
+#                                                     'zip_code': '12345'},
+#                                         'age': 27,
+#                                         'name': 'Jesse Bruce Pinkman',
+#                                         'phone': {'number': '555-5678'}}],
+#                            'name': 'Gustavo Fring'}
+#
+# print('Good')
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# (5)
+
+# Теперь давайте выполним сериализацию объектов, атрибутами которых могут быть другие объекты. Для этого переделайте
+# миксин JsonSerializableMixin, так чтобы он мог сериализовать такие объекты.
+# Внутри миксина JsonSerializableMixin обязательно должен быть метод to_json(), который возвращает итоговую строку
+# сериализации объекта. Все остальное вы можете создавать по своему усмотрению
+
+# import json
+#
+#
+# class JsonSerializableMixin:
+#     def to_json(self):
+#         return json.dumps(self.__dict__, default=lambda x: x.__dict__)
+#
+#
+# class Person(JsonSerializableMixin):
+#     def __init__(self, name, age, address):
+#         self.name = name
+#         self.age = age
+#         self.address = address
+#
+#
+# class Address(JsonSerializableMixin):
+#     def __init__(self, street, city, state, zip_code):
+#         self.street = street
+#         self.city = city
+#         self.state = state
+#         self.zip_code = zip_code
+#
+#
+# class Company(JsonSerializableMixin):
+#     def __init__(self, name, address):
+#         self.name = name
+#         self.address = address
+#
+#
+# address = Address("123 Main St", "Albuquerque", "NM", "987654")
+# print(address.to_json())
+# assert address.to_json() == '{"street": "123 Main St", "city": "Albuquerque", "state": "NM", "zip_code": "987654"}'
+#
+# walter = Person("Walter White", 30, address)
+# walter.hobby = ['Chemistry', 'Cooking']
+# walter.is_danger = True
+#
+# company_address = Address("3828 Piermont Dr", "Albuquerque", "NM", "12345")
+# walter.company = Company("SCHOOL", company_address)
+# assert walter.to_json() == '{"name": "Walter White", "age": 30, "address": {"street": "123 Main St", "city": "Albuquerque", "state": "NM", "zip_code": "987654"}, "hobby": ["Chemistry", "Cooking"], "is_danger": true, "company": {"name": "SCHOOL", "address": {"street": "3828 Piermont Dr", "city": "Albuquerque", "state": "NM", "zip_code": "12345"}}}'
+#
+# jesse_address = Address("456 Oak St", "Albuquerque", "NM", "12345")
+# jesse = Person("Jesse Bruce Pinkman", 27, jesse_address)
+# walter.is_lucky = False
+#
+# fring = Person("Gustavo Fring", 55, Address("Los Pollos Hermanos", "Albuquerque", "NM", "12345"))
+# fring.friends = [walter, jesse]
+#
+# assert fring.to_json() == '{"name": "Gustavo Fring", "age": 55, "address": {"street": "Los Pollos Hermanos", "city": "Albuquerque", "state": "NM", "zip_code": "12345"}, "friends": [{"name": "Walter White", "age": 30, "address": {"street": "123 Main St", "city": "Albuquerque", "state": "NM", "zip_code": "987654"}, "hobby": ["Chemistry", "Cooking"], "is_danger": true, "company": {"name": "SCHOOL", "address": {"street": "3828 Piermont Dr", "city": "Albuquerque", "state": "NM", "zip_code": "12345"}}, "is_lucky": false}, {"name": "Jesse Bruce Pinkman", "age": 27, "address": {"street": "456 Oak St", "city": "Albuquerque", "state": "NM", "zip_code": "12345"}}]}'
+# print('Good')
 
 
+# ----------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
+# (6)
